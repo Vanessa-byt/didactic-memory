40b574a9afed99cc4578153577b5d1421dce91ed
Vanessa-byt <24308060610012@cetis61.edu.mx>
2026-04-23 07:31:35 -0500

    hola3

--- practica1/app.py
+++ practica1/app.py
@@ -1,20 +1,37 @@
-from flask import Flask, render_template, url_for 
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+usuarios = {
+    "prueba@gmail.com": "oso1234",
+    "vaessita_1234@gmail.com": "vanessita1234"
+}
+
+@app.route("/")
 def inicio():
-    return render_template('nicio.html')
+    return render_template("nicio.html")
 
-@app.route('/crear_cuenta')
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if email in usuarios and usuarios[email] == password:
+
+        return render_template("gestor_tareas.html", usuario=email)
+    else:
+
+        return render_template("nicio.html", mensaje="Usuario no existe o contraseña incorrecta")
+
+
+@app.route("/formulario")
 def formulario():
-    return render_template('formulario.html')
+    return render_template("formulario.html")
 
-@app.route('/gestor_tareas')
+
+@app.route("/gestor_tareas")
 def gestor():
-    return render_template('gestor_tareas.html')
+    return render_template("gestor_tareas.html")
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
